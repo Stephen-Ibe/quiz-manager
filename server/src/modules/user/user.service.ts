7d693d9243c9da6acdf7978c1ec2f80287d8eab7
Stephen-Ibe <saintkalson@gmail.com>
2023-04-25 00:29:09 +0100

    User Registration Successful with Password hash

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UserRegisterRequestDto } from './dto/user-register.req.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   async handleUserRegistration(
     userData: UserRegisterRequestDto,
   ): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const password = await bcrypt.hash(userData.password, salt);
+
     const user = new User();
     user.name = userData.name;
     user.email = userData.email;
-    user.password = userData.password;
+    user.password = password;
 
     return await user.save();
   }
