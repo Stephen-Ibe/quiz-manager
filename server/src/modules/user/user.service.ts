0f54be4bf6e9b343b252062d79adba69df9faf9b
Stephen-Ibe <saintkalson@gmail.com>
2023-04-27 11:04:52 +0100

    hash password before signup

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,20 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { UserRegisterRequestDto } from './dto/user-register.req.dto';
 import { User } from './user.entity';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   async handleUserRegistration(
     userData: UserRegisterRequestDto,
   ): Promise<User> {
-    const salt = await bcrypt.genSalt();
-    const password = await bcrypt.hash(userData.password, salt);
-
     const user = new User();
     user.name = userData.name;
     user.email = userData.email;
-    user.password = password;
+    user.password = userData.password;
 
     return await user.save();
   }
