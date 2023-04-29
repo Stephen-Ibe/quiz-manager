0b3900ce18050670a418f6f29f1783baf5ebf44d
Stephen-Ibe <saintkalson@gmail.com>
2023-04-29 02:23:07 +0100

    created Migration files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfigAsync } from './config/typeorm.config';
+// import { typeOrmConfigAsync } from './config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
+import { dataSourceOptions } from './config/data-source';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    TypeOrmModule.forRoot(dataSourceOptions), // TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     QuizModule,
     UserModule,
   ],
