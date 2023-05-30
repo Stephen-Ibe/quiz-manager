e60ede2d70f6a41c9512ef5721c47b9c75944df5
Stephen-Ibe <saintkalson@gmail.com>
2023-05-30 20:06:31 +0100

    updated the user-register dto

--- server/src/modules/user/dto/user-register.req.dto.ts
+++ server/src/modules/user/dto/user-register.req.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
 import { MESSAGES, REGEX } from 'src/utils';
 
 export class UserRegisterRequestDto {
+  @ApiProperty({
+    description: 'User Name',
+    example: 'John Doe',
+  })
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'User Email',
+    example: 'john-doe@test.com',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'The password of the User',
+    example: 'Password@123',
+  })
   @IsNotEmpty()
   @Length(8, 24)
   @Matches(REGEX.PASSWORD_RULE, {
@@ -16,6 +29,10 @@ export class UserRegisterRequestDto {
   })
   password: string;
 
+  @ApiProperty({
+    description: 'Confirm the password',
+    example: 'Password@123',
+  })
   @IsNotEmpty()
   @Length(8, 24)
   @Matches(REGEX.PASSWORD_RULE, {
